c0bfb50a20fa35d7e80e5259170040607cea548c
[Clairinette] <[claire.crinier@gmail.com]>
2023-12-11 13:36:02 +0100

    correction problème du precedant commit (il manquait .matches) + ajout modal mentions legales

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,12 +30,10 @@ btnProjet.addEventListener('click', ()=> {
 const nav = document.getElementById('offcanvasNavbar');
 const li = document.querySelectorAll('li'); 
 
-function navOuverte() {
-    if (window.matchMedia("(min-width:1200px)")) {
-        nav.classList.add('show'); //la nav reste ouverte grâce à l'ajout de la class show
-        
-        li.forEach(function (allLi) { //selectionne tout les li avec le querySelectorAll
-            allLi.removeAttribute("data-bs-dismiss"); //enlève l'attribut data-bs-dismiss qui en mobile sert à ferme la nav quand on clique sur le lien
-        });
-    }
-}; navOuverte();
+if (window.matchMedia("(min-width:1200px)").matches) {
+    nav.classList.add('show'); //la nav reste ouverte grâce à l'ajout de la class show
+    
+    li.forEach(function (allLi) { //selectionne tout les li grace au foreach qui parcours le tableau (nodelist) de querySelectorAll (un tableau avec tout les li)
+        allLi.removeAttribute("data-bs-dismiss"); //enlève l'attribut data-bs-dismiss qui en mobile sert à ferme la nav quand on clique sur un lien
+    });
+};
