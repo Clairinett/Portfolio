0c9e13a5563e5aa5c6719232e5a0fe2016454909
[Clairinette] <[claire.crinier@gmail.com]>
2024-02-15 17:14:34 +0100

    suppression bouton formation dans realisation et code assigner (html,css,js) ajout realisation bdd animaux ajout lien github dans contact

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,19 @@
 const btnFormation = document.getElementById('btnFormation');
 const btnPerso = document.getElementById('btnPerso');
-const secFormation = document.getElementById('realisationsFormation');
 const secPerso = document.getElementById('realisationsPerso');
 const btnProjet = document.getElementById('btnProjet');
 const secProjet = document.getElementById('realisationsProjet');
 
-secFormation.style.display = "none";
 secProjet.style.display = "none";
-
-btnFormation.addEventListener('click', ()=> {
-    secFormation.style.display ="inline-flex";
-    secPerso.style.display = "none";
-    secProjet.style.display = "none";
-});
 
 btnPerso.addEventListener('click', ()=> {
     secPerso.style.display = "inline-flex";
-    secFormation.style.display ="none";
     secProjet.style.display ="none";
 });
 
 btnProjet.addEventListener('click', ()=> {
     secProjet.style.display = "inline-flex";
     secPerso.style.display = "none";
-    secFormation.style.display ="none";
 });
 
 
